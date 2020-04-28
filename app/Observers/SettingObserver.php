fb021639fc369ce8ba486b79b64cd345291f4544
Marcelo Corrêa <marcelocorrea229@gmail.com>
2020-04-28 12:59:27 -0300

    Update SettingObserver.php

--- app/Observers/SettingObserver.php
+++ app/Observers/SettingObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Events\SendMessagePusher;
 use App\Models\Setting;
 
 class SettingObserver
@@ -9,5 +10,11 @@ class SettingObserver
     public function creating(Setting $setting)
     {
         $setting->id = \App\Helpers\Models::generateUid($setting, 'id');
+    }
+
+    public function saved()
+    {
+        $data['action'] = 'change-settings';
+        event(new SendMessagePusher($data));
     }
 }
